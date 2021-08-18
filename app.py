ee83583e11d18d5be0f1dc23129288cfd56d1b09
Christoffer Boman <61548466+chrizboman@users.noreply.github.com>
2021-08-18 22:39:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request, jsonify
-import RPi.GPIO as GPIO
+from os import stat
+from flask import Flask, render_template, request, jsonify, send_from_directory
+import json, os
+#import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 #app.config["DEBUG"] = True
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
-status = 'astatus'
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
+# GPIO.setup(18, GPIO.OUT)
+status = None
 
 @app.route('/', methods=['GET'])
 def index():
@@ -15,9 +17,21 @@ def index():
 	
 	#return jsonify({"message": "Led successfully turned on" + str(status)})
 
-@app.route('/fan', methods=['GET'])
+@app.route('/fan/', methods=['GET'])
 def toggle():
 	status = request.args.get('status')
+
+	if status == 'off':
+		print("could do off code")
+
+	elif status == 'on':
+		print("and now some on code")
+
 	return jsonify({"message": "Led successfully turned on" + str(status)})
 
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
